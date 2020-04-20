9a0e23ca50eafa29726111f2775236940ef7d593
anbarasupr <anbarasu.2013@gmail.com>
2020-04-20 15:03:53 +0530

    update

--- 1-first-react-app/src/components/11-list-rendering/NameList.js
+++ 1-first-react-app/src/components/11-list-rendering/NameList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Person } from './Person';
 
 export const NameList = () => {
     const names = ['Bruce', 'Clark', 'Diana', 'Mark'];
@@ -24,11 +25,26 @@ export const NameList = () => {
             skill: 'React'
         }
     ];
-    const personsList = persons.map(person => (
+    /* const personsList = persons.map(person => (
         <h2>
             I am {person.name}, I am {person.age}, I know {person.skill}
         </h2>
-    ));
+    )); */
+
+    // Recommended way - separate the person rendering into separate js
+    /* 
+        Below code give a warning in console.
+        Warning: Each child in a list should have a unique "key" prop.
+        REACT need key property in the list of elements for efficient rendering purpose.
+     */
+    // const personsList = persons.map(person => <Person person={person} />);
+
+    /*
+         To solve above warning.
+         Also key is not accessible in child component and it is needed for react.
+         If we want that, we can send as a another props.
+    */
+    const personsList = persons.map(person => <Person key={person.id} person={person} />);
 
     return (
         <div>
@@ -40,3 +56,13 @@ export const NameList = () => {
         </div>
     )
 }
+
+
+
+/* 
+    Lis and Keys
+    1. A key is a special attribute you need to include when creating lists of elements
+    2. keys give the elements a stable identity
+    3. Keys help React identify which items have changed, are added or are removed
+    4. Help in efficient update of the user interface
+*/
---
+++ 1-first-react-app/src/components/11-list-rendering/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Person = ({person}) => { 
+    return (
+        <div>
+            <h2>
+               !!! I am {person.name}, I am {person.age}, I know {person.skill}
+            </h2>
+        </div>
+    )
+}
