f65ddc2bbb566f87d5e7e30ee9abb3477e26375b
anbarasu <anbarasu.2013@gmail.com>
2023-10-21 23:47:25 -0700

    update

--- 3-react-hooks-app/src/components/2-useEffect/2-run-effects-only-once/HookMouse.js
+++ 3-react-hooks-app/src/components/2-useEffect/2-run-effects-only-once/HookMouse.js
@@ -14,11 +14,13 @@ function HookMouse() {
 		console.log('useFffect called')
 		window.addEventListener('mousemove', logMousePosition)
 
+		// note that the useEffect cleanup function does not only run when our component wants to unmount,
+		//  it also runs right before the execution of the next scheduled effect.
 		return () => {
 			console.log('Component unmounting code for cleanup')
 			window.removeEventListener('mousemove', logMousePosition)
 		}
-	}, []);
+	}, []); 
 
 
 	console.log('HookMouse render called')
